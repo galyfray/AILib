ccb9cd0be7b071fe28dfd00c136c6ab8d42b3043
Galyfray <galyfray@gmail.com>
2021-08-29 22:11:38 +0200

    updated javadoc

--- src/main/java/fr/AILib/DataListener.java
+++ src/main/java/fr/AILib/DataListener.java
@@ -1,14 +1,15 @@
 package fr.AILib;
 
 /**
- * An interface that represent any objects that can receive data from a Data Sender
+ * An interface that represent any objects that can receive data from a {@link DataSender}
  */
 public interface DataListener {
 
     /**
      * The methode used by a data sender in order to save the data to the data listener
+     *
      * @param sender the object that send the Data
-     * @param data the data sent in order to be calculate or interpret
+     * @param data   the data sent in order to be calculate or interpret
      */
     void receive(DataSender sender, Object data);
 }
